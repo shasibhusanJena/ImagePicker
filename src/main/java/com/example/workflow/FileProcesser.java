2b946045842713dcb92a8d75816a3a5473eb7cd7
Shasibhusan Jena <shasibhusan.dev@gmail.com>
2024-05-13 17:50:31 +0530

    Store file in local loc

---
+++ src/main/java/com/example/workflow/FileProcesser.java
@@ -0,0 +1,30 @@
+package com.example.workflow;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+/**
+ * Class used to generate file and store in the temporary file location to process the file.
+ */
+public class FileProcesser {
+    String urlPath ="";
+    String userSelect ="";
+    public void downloadAndVerifyFile(String urlPath, String userSelect) throws IOException{
+           this.urlPath = urlPath ;
+           this.userSelect = userSelect;
+        try {
+            URL URL = new URL(urlPath);
+            BufferedImage img = ImageIO.read(URL);
+            File file = new File(userSelect+"_Downloaded.jpg");
+            ImageIO.write(img,"jpg",file);
+
+
+        }catch (Exception e){
+            System.out.println("file processing error occured"+e.getStackTrace());
+        }
+
+    }
+}
